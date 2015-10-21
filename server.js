f8871c505f049f9db78c33a5505c8b7dbce69098
dresosh <contact@dresosh.com>
2015-10-20 18:53:26 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express     = require( 'express' ),
+    app         = express(),
+    morgan      = require( 'morgan' ),
+    bodyParser  = require( 'body-parser' ),
+    port        = process.env.PORT || 3000,
+    mongoose    = require( 'mongoose' ),
+    DB          = 'mongodb://localhost.27017/basic_app'
+
+
+mongoose.connect( DB )
+
+app.get( '/', function( req, res ) {
+  res.send( '<h1>This is the home page</h1>' )
+})
+
+
+
+
+
+
+app.listen( port, function() {
+  console.log(' You are currently running on Port ' + port )
+})
